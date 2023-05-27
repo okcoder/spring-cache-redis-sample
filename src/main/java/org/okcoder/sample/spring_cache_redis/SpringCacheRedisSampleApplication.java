82b6a7b8e3d2ff936ce28902daee73abc27cce58
okcoder <okcoder@live.com>
2023-05-27 10:05:06 +0900

    create project from start.spring.io

---
+++ src/main/java/org/okcoder/sample/spring_cache_redis/SpringCacheRedisSampleApplication.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_cache_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheRedisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheRedisSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/okcoder/sample/spring_cache_redis/SpringCacheRedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_cache_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheRedisSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
